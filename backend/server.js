ed6621fba527e5a88118b05dde22d21cdae4aa89
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2023-06-26 19:44:36 +0700

    update mongo

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,28 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+//Routes
+app.get("/", (req, res) => {
+  res.send("Home page");
 });
+
+// Create a Task
+app.post("/api/tasks", async (req, res) => {
+  console.log(req.body);
+  res.send("Task Created");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
