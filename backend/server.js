7f28bc4e3903801cd6120e74fbc85a69bb448ccf
DuyVu285 <80942615+DuyVu285@users.noreply.github.com>
2023-06-28 14:55:17 +0700

    update routes

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,22 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
+
+const taskRoutes = require("./routes/taskRoute");
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-// Create a Task
-app.post("/api/tasks", async (req, res) => {
-  console.log(req.body);
-  res.send("Task Created");
-});
+app.use(taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
